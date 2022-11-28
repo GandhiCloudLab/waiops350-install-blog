bf5fd2140b7fc16d0c724dc6848b3f7cc5f9a068
Cloud-Native Toolkit <GandhiCloudLab@gmail.com>
2022-11-28 15:31:51 +0530

    intial files

---
+++ 02-install-eventmgr-on-roks/files/00-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Entitlement key
+export ENTITLEMENT_KEY=eyJhbG...........................e4Zog
+
+export EVENT_MANAGER_VERSION=1.6.6
+
+
+export EVENT_MANAGER_CHANNEL=v1.10
+
+export EVENT_MANAGER_STARTING_CSV=noi.v1.6.0
+
+export EVENT_MANAGER_NAMESPACE=cp4waiops-emgr
+
+export EVENT_MANAGER_STORAGE_CLASS=ibmc-file-gold-gid
+
+
+
+
+
---
+++ 03-install-infra-automation-on-roks/files/00-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Entitlement key
+export ENTITLEMENT_KEY=eyJ0eX.......VwYu0
+
+export SUBSCRIPTION_CHANNEL_IA=v3.5
+export SUBSCRIPTION_CHANNEL_IAF=v1.3
+export NAMESPACE=cp4waiops
+export SUBSCRIPTION_STARTINGCSV_IA=ibm-infrastructure-automation-operator.v3.5.0
+export SUBSCRIPTION_STARTINGCSV_IAF=ibm-automation.v1.3.10
+
+export STORAGE_CLASS=ibmc-file-gold-gid
+export STORAGE_CLASS_LARGE_BLOCK=ibmc-block-gold
+
+export YOUR_LDAP_USER_GROUP=imgroup
